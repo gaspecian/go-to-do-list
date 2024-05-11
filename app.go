5a2d51ea3a473cca2ceedc534de3b3e31b7a90e3
Gabriel Specian <ga.specian@gmail.com>
2024-05-11 20:40:12 -0300

    chore: create http server

--- app.go
+++ app.go
@@ -1,3 +1,20 @@
 package main
 
-func main() {}
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+func main() {
+	httpServer := &http.Server{
+		Addr:           ":8080",
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	if err := httpServer.ListenAndServe(); err != nil {
+		log.Fatalf("Failed to listen and serve: %+v", err)
+	}
+}
