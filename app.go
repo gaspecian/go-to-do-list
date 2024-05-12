1450f5b5bcfc80299bde2bc40ad5016d242bb373
Gabriel Specian <ga.specian@gmail.com>
2024-05-11 22:53:22 -0300

    chore: add router and logger request

--- app.go
+++ app.go
@@ -1,17 +1,43 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 	"time"
+
+	"github.com/gaspecia/go-to-do-list/middleware"
+	"github.com/gorilla/mux"
 )
 
+type Info struct {
+	Name    string `json:"name"`
+	Version string `json:"version"`
+}
+
+func InfoHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	response := Info{
+		Name:    "to-do-api",
+		Version: "1.0.0",
+	}
+	json.NewEncoder(w).Encode(response)
+}
+
 func main() {
+	router := mux.NewRouter()
+	router.Use(
+		middleware.LogginMiddleware(log.Default()),
+	)
+
+	router.HandleFunc("/", InfoHandler)
+
 	httpServer := &http.Server{
 		Addr:           ":8080",
 		ReadTimeout:    10 * time.Second,
 		WriteTimeout:   10 * time.Second,
 		MaxHeaderBytes: 1 << 20,
+		Handler:        router,
 	}
 
 	if err := httpServer.ListenAndServe(); err != nil {
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/gaspecia/go-to-do-list
 
 go 1.21.6
+
+require (
+	github.com/gorilla/mux v1.8.1 // indirect
+)
