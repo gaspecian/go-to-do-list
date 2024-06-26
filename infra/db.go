614fc3d1fce2588e6e36eef7e3ca6d4e3fd2db0c
gaspecian <ga.specian@gmail.com>
2024-06-25 23:16:48 -0300

    fix: ajustar Fatalf logger

--- infra/db.go
+++ infra/db.go
@@ -14,7 +14,7 @@ func DbConnect() *sql.DB {
 	connStr := fmt.Sprintf("postgresql://%s:%s@%s:%s/%s?sslmode=disable", dbConfig.User, dbConfig.Password, dbConfig.Host, dbConfig.Port, dbConfig.Database)
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
-		log.Fatalf("Error while connecting to DB:", err)
+		log.Fatalf("Error while connecting to DB: %s", err)
 	}
 
 	return db
