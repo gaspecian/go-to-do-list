b834479e9465529d7b91aba5bd2035e2ae366f88
Gabriel Specian <ga.specian@gmail.com>
2024-06-25 23:08:29 -0300

    WIP

---
+++ infra/db.go
@@ -0,0 +1,21 @@
+package infra
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	"github.com/gaspecian/go-to-do-list/config"
+)
+
+func DbConnect() *sql.DB {
+	dbConfig := config.LoadEnv().DbConnection
+
+	connStr := fmt.Sprintf("postgresql://%s:%s@%s:%s/%s?sslmode=disable", dbConfig.User, dbConfig.Password, dbConfig.Host, dbConfig.Port, dbConfig.Database)
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatalf("Error while connecting to DB:", err)
+	}
+
+	return db
+}
